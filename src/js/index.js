4d4f39a5ba646d7f0656cebea9cd6d07c8c72a02
elvvira <elvvira.castellanos@gmail.com>
2023-01-26 23:47:14 +0100

    CASA

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,24 @@ const containerSubmit = document.getElementById('container-submit');
 const containerThanks = document.getElementById('container-thanks');
 const formElement = document.getElementById('form');
 const button = document.getElementById('button');
+const youSelect = document.getElementById('you-select');
+
+formElement.addEventListener('click', e => {
+  if (e.target.name === 'number') {
+    youSelect.textContent = e.target.previousElementSibling.textContent + ' ';
+  }
+  return;
+});
 
 formElement.addEventListener('submit', e => {
   e.preventDefault();
 });
+
 button.addEventListener('click', e => {
-  console.dir(e.target);
-  containerSubmit.classList.add('none');
-  containerThanks.classList.add('block');
+  for (let index = 0; index < 5; index++) {
+    if (formElement.children[index].lastElementChild.checked) {
+      containerSubmit.classList.add('none');
+      containerThanks.classList.add('block');
+    }
+  }
 });
