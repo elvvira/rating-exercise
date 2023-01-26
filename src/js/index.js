4eefb72c6b2560c46e95f3d2347f3bd3a55d3d80
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-01-26 15:04:23 +0100

    clase

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
+// import { sayHello } from './demo.js';
 
-sayHello();
+const containerSubmit = document.getElementById('container-submit');
+const containerThanks = document.getElementById('container-thanks');
+const formElement = document.getElementById('form');
+const button = document.getElementById('button');
+
+formElement.addEventListener('submit', e => {
+  e.preventDefault();
+});
+button.addEventListener('click', e => {
+  console.dir(e.target);
+  containerSubmit.classList.add('none');
+  containerThanks.classList.add('block');
+});
